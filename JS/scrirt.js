28bb22ade399973557069e8b42546036cdece2b6
7Oleksandr7 <040818a@gmail.com>
2023-01-17 20:56:30 +0200

    1

--- JS/scrirt.js
+++ JS/scrirt.js
@@ -59,3 +59,58 @@
 //   buttonEl.textContent = label;
 //   container.appendChild(buttonEl);
 // });
+
+// const openModalBtn = document.getElementById("openModalBtn");
+// const overlay = document.getElementById("overlay");
+// const closeBtn = document.getElementById("closeBtn");
+// const openClass = "open";
+
+// openModalBtn.addEventListener("click", toggleModal);
+// overlay.addEventListener("click", toggleModal);
+// closeBtn.addEventListener("click", toggleModal);
+
+// // function handleModal() {
+// //   console.log("Hello world!");
+// // }
+
+// function toggleModal() {
+//   document.body.classList.toggle(openClass);
+// }
+
+const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+// Some helper functions to render palette items
+createPaletteItems();
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRandomColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+function getRandomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, "0");
+}
